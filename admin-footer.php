9b8307ce918b388eff1ce54a2fb825d7fd468a11
Shanis-Nheralat <shanisnheralat@gmail.com>
2025-04-29 18:26:14 +0400

    Update admin-footer.php

--- admin-footer.php
+++ admin-footer.php
@@ -1,27 +1,18 @@
 <?php
 /**
- * Admin Footer Component
- * This file contains the footer for the admin panel
+ * Common footer elements
  */
-
-// Set current year
-$current_year = date('Y');
-
-// Set company name
-$company_name = "Backsure Global Support";
-
-// Set version
-$admin_version = "1.0";
 ?>
-<!-- Admin Footer -->
-<footer class="admin-footer">
-  <div class="footer-left">
-    <p>&copy; <?php echo $current_year; ?> <?php echo htmlspecialchars($company_name); ?>. All rights reserved.</p>
-  </div>
-  <div class="footer-right">
-    <span>Admin Panel v<?php echo htmlspecialchars($admin_version); ?></span>
-  </div>
-</footer>
-
-<!-- Common JavaScript Files -->
-<script src="assets/js/admin-core.js"></script>
+    <!-- Admin Footer -->
+    <footer class="admin-footer">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-6 footer-left">
+            <p>&copy; <?php echo date('Y'); ?> Backsure Global Support. All rights reserved.</p>
+          </div>
+          <div class="col-md-6 footer-right text-end">
+            <span>Admin Panel v1.0</span>
+          </div>
+        </div>
+      </div>
+    </footer>
